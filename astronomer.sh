30d5964a98addc206c374284d05734862c5a3f89
n1v0lg <n1v0lg@users.noreply.github.com>
2021-05-10 20:52:47 +0200

    Plug in planets url

--- astronomer.sh
+++ astronomer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PLANETS_URL="https://raw.githubusercontent.com/dariusk/corpora/master/data/science/planets.json"
+PLANETS_URL="https://raw.githubusercontent.com/n1v0lg/astronomer/main/planets.json"
 MOONS=($(curl -s "${PLANETS_URL}" | jq '.planets | .[] | .moons | select(length > 0)' | jq -s 'add | @sh' | tr -d \'\"))
 MOON=${MOONS[$RANDOM % ${#MOONS[@]}]}
 echo You shall name you moon "${MOON}".
