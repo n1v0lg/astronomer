98c157a6788fa81e8342780bb67aa171a74562b2
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2021-05-11 10:11:35 +0200

    Update whatdoiget.sh

--- whatdoiget.sh
+++ whatdoiget.sh
@@ -3,4 +3,4 @@
 PLANETS_URL="https://raw.githubusercontent.com/n1v0lg/astronomer/main/planets.json"
 MOONS=($(curl -s "${PLANETS_URL}" | jq '.planets | .[] | .moons | select(length > 0)' | jq -s 'add | @sh' | tr -d \'\"))
 MOON=${MOONS[$RANDOM % ${#MOONS[@]}]}
-echo You shall name you moon "${MOON}".
+echo You shall name your moon "${MOON}".
